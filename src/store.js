5a4d98c4ea89ab214e7048449b4a38aa77a3f3d5
Sunhye Kwon <kwonsunhye0206@gmail.com>
2018-11-18 22:43:31 -0500

    practise redux-promise-middleware

--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
 import {createLogger} from 'redux-logger';
-import ReduxThunk from 'redux-thunk';
-
+//import ReduxThunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise-middleware';
 const logger = createLogger();
+const pm = promiseMiddleware({
+    promiseTypeSuffixes:['PENDING','SUCCESS','FAILURE']
+});
 
-const store = createStore(modules, applyMiddleware(logger,ReduxThunk))
+const store = createStore(modules, applyMiddleware(logger,pm))
 
 export default store;
